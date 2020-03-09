a4296c19e24157afa6ef6abbdb1d0361b9237594
zjyl1994 <me@zjyl1994.com>
2020-03-09 09:40:16 +0800

    add shortcode key

--- js/app.js
+++ js/app.js
@@ -2,19 +2,21 @@ window.addEventListener('DOMContentLoaded', function () {
     var timer = setInterval(updateRemaining, 1000);
     var mainlist = document.getElementById('authcodes');
     var authcodes = [], selectIndex = 0;
+    var shortCodeBuffer = '';
+    var shortCodeActive = false;
     // load data.json
     fetch('data/data.json')
-    .then(res => res.json())
-    .then((out) => {
-        authcodes = out;
-        refreshCodeList();
-        updateRemaining();
-    });
+        .then(res => res.json())
+        .then((out) => {
+            authcodes = out;
+            refreshCodeList();
+            updateRemaining();
+        });
     // functions
-    function updateRemaining(){
+    function updateRemaining() {
         let remain = window.otplib.authenticator.timeRemaining();
         document.getElementById('softkey-center').innerText = `Next in ${remain}s`;
-        if(remain === 30){
+        if (remain === 30) {
             refreshCodeList();
         }
     }
@@ -30,8 +32,8 @@ window.addEventListener('DOMContentLoaded', function () {
         mainlist.children[selectIndex].classList.add('active');
     }
     function selectItemByIndex() {
-        [].forEach.call(mainlist.children, function(el) {
-          el.classList.remove('active');
+        [].forEach.call(mainlist.children, function (el) {
+            el.classList.remove('active');
         });
         let activeElem = mainlist.children[selectIndex];
         activeElem.classList.add('active');
@@ -53,6 +55,68 @@ window.addEventListener('DOMContentLoaded', function () {
                 selectIndex++;
                 if (selectIndex > (authcodes.length - 1)) selectIndex = 0;
                 selectItemByIndex();
+                break;
+            case 'SoftLeft':
+                console.log("Left press");
+                break;
+            case 'SoftRight':
+                console.log("Right press");
+                break;
+            case 'Enter':
+                console.log("Enter press");
+                break;
+            case '1':
+                shortCodeBuffer += '1';
+                break;
+            case '2':
+                shortCodeBuffer += '2';
+                break;
+            case '3':
+                shortCodeBuffer += '3';
+                break;
+            case '4':
+                shortCodeBuffer += '4';
+                break;
+            case '5':
+                shortCodeBuffer += '5';
+                break;
+            case '6':
+                shortCodeBuffer += '6';
+                break;
+            case '7':
+                shortCodeBuffer += '7';
+                break;
+            case '8':
+                shortCodeBuffer += '8';
+                break;
+            case '9':
+                shortCodeBuffer += '9';
+                break;
+            case '0':
+                shortCodeBuffer += '0';
+                break;
+            case '*':
+                shortCodeBuffer += '*';
+                break;
+            case '#':
+                shortCodeBuffer += '#';
+                if(shortCodeActive){
+                    switch(shortCodeBuffer){
+                        case "*#0000#":
+                            console.log("about");
+                            break;
+                        case "*#467678#":
+                            console.log("import");
+                            break;
+                        case "*#397678#":
+                            console.log("export");
+                            break;
+                    }
+                    shortCodeBuffer = '';
+                    shortCodeActive = false;
+                }else{
+                    shortCodeActive = true;
+                }
                 break;
         }
     });
