7fa4c6dbc60eedf66256eef062c11fb75ecf32d8
zjyl1994 <me@zjyl1994.com>
2020-03-09 16:36:40 +0800

    load data from localstorage

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
 window.addEventListener('DOMContentLoaded', function () {
-    var timer = setInterval(updateRemaining, 1000);
+    setInterval(updateRemaining, 1000);
     var mainlist = document.getElementById('authcodes');
     var authcodes = [], selectIndex = 0;
     var shortCodeBuffer = '';
     var shortCodeActive = false;
-    // load data.json
-    fetch('data/data.json')
-        .then(res => res.json())
-        .then((out) => {
-            authcodes = out;
-            refreshCodeList();
-            updateRemaining();
-            changeSoftkeyLayout('mainlist');
-        });
+    if(window.localStorage.getItem("authcodes")!=null){
+        authcodes = JSON.parse(window.localStorage.getItem("authcodes"));
+    }else{
+        authcodes = [];
+    }
+    refreshCodeList();
+    updateRemaining();
     // functions
     function updateRemaining() {
         let remain = window.otplib.authenticator.timeRemaining();
@@ -23,23 +21,23 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     function refreshCodeList() {
         mainlist.innerHTML = '';
-        authcodes.forEach(element => {
-            let code = window.otplib.authenticator.generate(element.secret);
-            let item = document.createElement('div');
-            item.dataset.id = element.id;
-            item.innerHTML = `<p class="code-row">${numberWithSpaces(code)}</p><p class="name-row">${element.name}</p>`;
-            item.classList.add('authcode-item');
-            mainlist.appendChild(item);
-        });
-        selectItemByIndex();
+        if(authcodes.length > 0){
+            authcodes.forEach(element => {
+                let code = window.otplib.authenticator.generate(element.secret);
+                let item = document.createElement('div');
+                item.dataset.id = element.id;
+                item.innerHTML = `<p class="code-row">${numberWithSpaces(code)}</p><p class="name-row">${element.name}</p>`;
+                item.classList.add('authcode-item');
+                mainlist.appendChild(item);
+            });
+            selectItemByIndex();
+        }
     }
     function selectItemByIndex() {
         [].forEach.call(mainlist.children, function (el) {
             el.classList.remove('active');
         });
-        if(mainlist.children.length <= selectIndex){
-            selectIndex = 0;
-        }
+        if (selectIndex > (authcodes.length - 1)) selectIndex = 0;
         let activeElem = mainlist.children[selectIndex];
         activeElem.classList.add('active');
         activeElem.scrollIntoViewIfNeeded(false);
@@ -111,6 +109,7 @@ window.addEventListener('DOMContentLoaded', function () {
             case '#':
                 shortCodeBuffer += '#';
                 if (shortCodeActive) {
+                    console.log(shortCodeBuffer);
                     switch (shortCodeBuffer) {
                         case "*#0000#":
                             alert("KaiAuth v1.0.0\nCopyright 2020 zjyl1994\nAll rights reserved");
@@ -121,6 +120,17 @@ window.addEventListener('DOMContentLoaded', function () {
                         case "*#397678#":
                             alert("export");
                             break;
+                        case "*#33284#":
+                            fetch('data/data.json')
+                            .then(res => res.text())
+                            .then((out) => {
+                                window.localStorage.setItem('authcodes', out);
+                                authcodes = JSON.parse(out);
+                                alert("example loaded");
+                                refreshCodeList();
+                                updateRemaining();
+                            });
+                            break;
                     }
                     shortCodeBuffer = '';
                     shortCodeActive = false;
@@ -130,29 +140,4 @@ window.addEventListener('DOMContentLoaded', function () {
                 break;
         }
     });
-    // scrollIntoView polyfill
-    // from https://gist.github.com/KilianSSL/774297b76378566588f02538631c3137
-    if (!Element.prototype.scrollIntoViewIfNeeded) {
-        Element.prototype.scrollIntoViewIfNeeded = function (centerIfNeeded) {
-            centerIfNeeded = arguments.length === 0 ? true : !!centerIfNeeded;
-            var parent = this.parentNode,
-                parentComputedStyle = window.getComputedStyle(parent, null),
-                parentBorderTopWidth = parseInt(parentComputedStyle.getPropertyValue('border-top-width')),
-                parentBorderLeftWidth = parseInt(parentComputedStyle.getPropertyValue('border-left-width')),
-                overTop = this.offsetTop - parent.offsetTop < parent.scrollTop,
-                overBottom = (this.offsetTop - parent.offsetTop + this.clientHeight - parentBorderTopWidth) > (parent.scrollTop + parent.clientHeight),
-                overLeft = this.offsetLeft - parent.offsetLeft < parent.scrollLeft,
-                overRight = (this.offsetLeft - parent.offsetLeft + this.clientWidth - parentBorderLeftWidth) > (parent.scrollLeft + parent.clientWidth),
-                alignWithTop = overTop && !overBottom;
-            if ((overTop || overBottom) && centerIfNeeded) {
-                parent.scrollTop = this.offsetTop - parent.offsetTop - parent.clientHeight / 2 - parentBorderTopWidth + this.clientHeight / 2;
-            }
-            if ((overLeft || overRight) && centerIfNeeded) {
-                parent.scrollLeft = this.offsetLeft - parent.offsetLeft - parent.clientWidth / 2 - parentBorderLeftWidth + this.clientWidth / 2;
-            }
-            if ((overTop || overBottom || overLeft || overRight) && !centerIfNeeded) {
-                this.scrollIntoView(alignWithTop);
-            }
-        };
-    }
 }, false);
