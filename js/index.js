2de1825429d6f65f1215c2423be027dd7fc3ecb1
LiarOnce <qq1062787843@hotmail.com>
2020-03-10 15:36:52 +0800

    Add a translatable field.

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     function updateRemaining() {
         let remain = window.otplib.authenticator.timeRemaining();
-        document.getElementById('softkey-center').innerText = `Next in ${remain}s`;
+        document.getElementById('softkey-center').innerText = `${remain}s`;
         if (remain === 30) {
             refreshCodeList();
         }
